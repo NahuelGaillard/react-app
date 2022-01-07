499b32a7a4b1ba8d7553b0e6c47e431b200d6199
NahuelGaillard <nahuelgaillard18@hotmail.com>
2022-01-07 14:09:51 -0300

    arreglo

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { context } from "./CardContext";
 import { useContext } from "react";
+import { Link } from 'react-router-dom';
 
 
 
@@ -23,21 +24,22 @@ const Cart = () => {
             <div className="modalCarrito">
             <h3>Carrito</h3>
             <button id="carritoCerrar"><i className="fas fa-times-circle"></i></button>
-                {carrito.map((ItemCarrito) =>{
-                    console.log(carrito);
+
+            {carrito.length > 0 ? <div>{carrito.map((ItemCarrito) =>{
                     
                     return (
                       <>
-                        {carrito.length > 0  ? <div key={ItemCarrito.id} className='productoEnCarrito'>
+                        <div key={ItemCarrito.id} className='productoEnCarrito'>
                             <p>{ItemCarrito.nombre}</p>
                             <p>Precio: ${ItemCarrito.precio}</p>
-                            <p>Cantidad: ${ItemCarrito.cantidad}</p>
+                            <p>Cantidad: {ItemCarrito.cantidad}</p>
                             <button onClick={() => eliminarProducto(ItemCarrito.id)} className="botonEliminar"><i className="fas fa-trash"></i></button>
-                        </div> : <p>Cargando...</p>  }
+                        </div>  
                       </>
 
                     )
-                })}
+                })}</div> : <Link to="/">Empezar a comprar</Link> }
+                
                     <p className="precioProducto">Precio total: $ {precioTotal}</p>
                     {/* <button className="miBoton w-100" onClick="finalizarCompra()">Finalizar Compra <i className= "fas fa-shopping-basket"></i></button> */}
                 </div>
